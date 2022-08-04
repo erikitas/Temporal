f4af96cc2c242f490da0b4ba4d032112c7d49ad3
erikitas <erikathompson818@gmail.com>
2022-08-04 09:26:26 -0300

    mei caminho andado

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import { React } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  
+      <View style={styles.bloco}>
+        <Text style={styles.titulo}>Meteorologia</Text>
+      </View>
+  
+      <View>
+        <Text>Cidade:</Text>
+        <TextInput
+          placeholder='Digite sua cidade'
+          style={styles.input}
+        />
+      </View>
+
+      <View>
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.textoBloco}>Procurar</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
@@ -13,8 +31,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F9F5EB',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  bloco: {
+    alignItems: 'center',
+    justifyContent: 'center',
+	  width: '60%',
+	  backgroundColor: '#C6DCE4',
+    borderRadius: 5,
+    },
+
+    textobloco: {
+      color: '#fff',
+      fontSize: 20,
+      padding: 20,
+    },
+  
+    botao: {
+      padding: 20 ,
+    },
+  
+    textobotao:{
+      color: '#fff',
+      fontSize: 25,
+  
+    },
+  }); 
