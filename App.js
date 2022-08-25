06ea601eb107bf5920da55408e34349336959115
erikitas <erikathompson818@gmail.com>
2022-08-25 09:36:14 -0300

    api finalizada

--- App.js
+++ App.js
@@ -1,40 +1,42 @@
-import { React } from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View,TextInput, TouchableOpacity } 
 from 'react-native';
 import Tempo from './components/Tempo';
 import Api from './components/Api';
 
 export default function App() {
-  const [dados, setDados] = useState();
-  const [cidade, setCidade] = useState();
+  const [cidade, setCidade] = useState("peruibe");
+  const [dados, setDados] = useState("");
 
   async function buscaCep(){
-    const resposta= await Api.get('weather?array_limit=1&fields=only_results,temp,city_name,description,forecast,max,min,date&key=11568a6c&city_name=CapaoRedondo,SP');
-    setDados(resposta.data.forecast[0]);
+    const response = await Api.get(`weather?array_limit=2&fields=only_results,temp,city_name,forecast,max,min,date,description&key=11568a6c&city_name=${cidade},SP`);
+    setDados(response.data.forecast[0]);
   }
 
   return (
     <View style={styles.container}>
-  
       <View style={styles.bloco}>
+
         <Text style={styles.titulo}>Meteorologia</Text>
-      </View>
+
   
       <View>
+      <View style={styles.blocoGeral}>
         <Text style={styles.cidade}>Cidade:</Text>
         <TextInput
           placeholder='Digite sua cidade'
           style={styles.input}
-          onKeyPress={buscaCep}
+          onChangeText={(value)=>setCidade(value)}
         />
       </View>
 
-      <View style={styles.blocoGeral}>
-        <TouchableOpacity style={styles.botao}>
-          <Text style={styles.textoBloco}>Procurar</Text>
-        </TouchableOpacity>
+        <TouchableOpacity style={styles.botao} onPress={buscaCep}>
+          <Text style={styles.textoBotao}>Procurar</Text>
+        </TouchableOpacity> 
+
       </View>
-    <Tempo data={dados}/>
+    <Tempo data={dados}/> 
+       </View>
     </View>
   );
 }
@@ -42,30 +44,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F9F5EB',
+    backgroundColor: '#B7CADB',
     alignItems: 'center',
     justifyContent: 'center',
   },
   bloco: {
     alignItems: 'center',
     justifyContent: 'center',
-	  width: '55%',
-    height: '5%',
-	  backgroundColor: '#C6DCE4',
+	  width: 300,
+	  backgroundColor: '#92A9BD',
     borderRadius: 5,
+    },
+    titulo:{
+      fontSize: 25,
+      marginBottom: 15,
+      marginTop: 10,
+      fontWeight: 'bold',
+      color: "#1C3879",
     },
 
     textobloco: {
       color: '#fff',
-      fontSize: 45,
-      padding: 20,
+		  fontSize: 30,
+		  padding: 20,
     },
     input:{
-      fontSize: 15,
+        width: '80%',	
+        color: '#fff',
+        borderBottomWidth:1,
     },
   
     botao: {
-      padding: 20 ,
+      padding: 10,
+      alignItems: 'center',
+      marginTop: 10,
+      borderRadius: 9,
+      backgroundColor: '#607EAA'
     },
   
     textobotao:{
--- components/Tempo.js
+++ components/Tempo.js
@@ -4,9 +4,9 @@ import { Text, StyleSheet } from 'react-native';
 export default function Tempo(props) {
  return (
      <>
-        <Text style={styles.texto}>Minimo:{props.data.min}</Text>
-        <Text style={styles.texto}>Maximo:</Text>
-        <Text style={styles.texto}>Descricao:</Text>
+        <Text style={styles.texto}>Minimo: {props.data.min}</Text>
+        <Text style={styles.texto}>Maximo: {props.data.max}</Text>
+        <Text style={styles.texto}>Descricao: {props.data.description}</Text>
     </>
     );
  }
